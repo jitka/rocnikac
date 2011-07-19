b87795509bab6245764da19a97944c8adfffd3ff
Jitka Novotná <jitka@ucw.cz>
2011-07-19 23:01:21 +0200

    pridana normalizacni funckce ale jeste by to chtelo ohodnocovat

--- main.c
+++ main.c
@@ -17,6 +17,12 @@ uint short_to_long[32];
 uint long_to_short[64];
 #endif
 
+static inline void swap_uint(uint *x, uint *y){
+	uint tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
 static inline void binary_luint(luint n){
 	for (int i = 0; i < 64; i++){
 		if (i%5==0)
@@ -30,15 +36,12 @@ static inline void binary_uint(uint n){
 		printf("%d", !!(n&(1<<i)) );
 }
 
-static inline void set_edge_color(int player, luint old[4], luint new[4], int u, int v){
+static inline void set_edge_color(int player, luint old[4], luint new[4], uint u, uint v){
 	//zeleny 10 cerveny 01 zadna 00
 	for (int i = 0; i < 4; i++)
 		new[i]=old[i];
-	if ( u < v ) {
-		int tmp = v;
-		v = u;
-		u = tmp;
-	}
+	if ( u < v )
+		swap_uint(&u,&v);
 	int pos = (u*(u+1))/2 + v;
 #if N > 16
 	int pos5 = (pos/3)*5; //zacatek klicovich peti bitu
@@ -87,6 +90,67 @@ void print_adjacency_matrix(luint m[4],char *text){
 	fclose(F);
 }
 
+static inline void evaluation_1(uint m[N][N],uint evaluation[N]){
+	m[0][0]=m[0][0];
+	for (int i=0; i<N; i++)
+		evaluation[i] = 0;
+}
+
+static inline void evaluation_2(uint m[N][N],uint evaluation[N]){
+	for (int i=0; i<N; i++){
+		evaluation[i] = 0u;
+		for (int j=0; j<N; j++)
+			evaluation[i] += m[i][j];
+
+	}
+}
+
+static inline void normalization(luint m[4]){
+	//prevede m na matici sousednosti
+	uint adjacency_matrix[N][N];
+	for (int i=0; i<N; i++)
+		for (int j=0; j<N; j++)
+			adjacency_matrix[i][j] = get_edge_color(&m[0],i,j);
+
+	//ohodnoti vrcholy
+	uint evaluation[N];
+	evaluation_2(adjacency_matrix,evaluation);
+
+	for (int i=0; i<N; i++)
+		printf("%d ",evaluation[i]);
+	printf("\n");
+
+	//tridit zaroven permutace a matici
+	inline void swap(int x, int y){
+		//sloupce
+		for (int i=0; i<N; i++)
+			swap_uint(&adjacency_matrix[i][x],&adjacency_matrix[i][y]);
+		//radky
+		for (int i=0; i<N; i++)
+			swap_uint(&adjacency_matrix[x][i],&adjacency_matrix[y][i]);
+		swap_uint(&evaluation[x], &evaluation[y]);
+	}
+	int unsorted = TRUE;
+	for (int i = 0; i<N && unsorted; i++){
+		unsorted = FALSE;
+		for (int j=i+1; j<N; j++){
+			if (evaluation[i] > evaluation[j])
+				swap(i,j);
+			if (evaluation[j-1] > evaluation[j])
+				unsorted = TRUE;
+		}
+	}
+	
+	
+	//prevest zpet
+	for (int i=0; i<4; i++)
+		m[i]=0llu;
+	for (uint i=0; i<N; i++)
+		for (uint j=0; j<N; j++)
+			if (adjacency_matrix[i][j]>0)
+				set_edge_color(adjacency_matrix[i][j]-1,&m[0],&m[0],i,j);
+	
+}
 
 int main(){
 
@@ -115,7 +179,11 @@ int main(){
 	timer++;
 	print_adjacency_matrix(m,"");
 
-	set_edge_color(RED,m2,m,12,4);
+	set_edge_color(RED,m2,m,12,2);
+	timer++;
+	print_adjacency_matrix(m,"");
+
+	normalization(m);
 	timer++;
 	print_adjacency_matrix(m,"");
 
