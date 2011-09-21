7d2df67119d9c605af0adf667ac9ad59e1d06d3e
Jitka Novotna <jitka@ucw.cz>
2011-09-21 09:42:29 +0200

    po dlouhem case. funkcni?

--- main.c
+++ main.c
@@ -136,6 +136,71 @@ static inline void evaluation_2(uint m[N][N],uint evaluation[N]){
 			evaluation[i] += m[i][j];
 
 	}
+
+}
+
+static inline void evaluation_3(uint m[N][N],uint evaluation[N]){
+	//zalozene na trojuhelinikach viz papir u pocitace
+	uint green[N],red[N]; //sousedi
+	uint g=0,r=0;
+	for (int i=0; i<N; i++){
+		evaluation[i] = 0u;
+		//seznam sousedu danych barev
+		for (int j=0; j<N; j++){
+			if (j == i)
+				break;
+			if (m[i][j] == GREEN)
+				green[g++] = j;
+			if (m[i][j] == GREEN)
+				red[r++] = j;
+		}
+		for (uint x = 0; x < g; x++)
+			for (uint y = x+1; y < g; y++)
+				switch (m[x][y]){
+				case 0: 
+					evaluation[i] += 1;
+					break;
+				case GREEN: 
+					evaluation[i] += N;
+					break;
+				case RED:
+					evaluation[i] += N*N;
+					break;
+				}
+		for (uint x = 0; x < g; x++)
+			for (uint y = 0; y < r; y++)
+				switch (m[x][y]){
+				case 0: 
+					evaluation[i] += N*N*N;
+					break;
+				case GREEN: 
+					evaluation[i] += N*N*N*N;
+					break;
+				case RED:
+					evaluation[i] += N*N*N*N*N;
+					break;
+				}
+		for (uint x = 0; x < r; x++)
+			for (uint y = x+1; y < r; y++)
+				switch (m[x][y]){
+				case 0: 
+					evaluation[i] += N*N*N*N*N*N;
+					break;
+				case GREEN: 
+					evaluation[i] += N*N*N*N*N*N*N;
+					break;
+				case RED:
+					evaluation[i] += N*N*N*N*N*N*N*N;
+					break;
+				}
+
+
+
+
+
+
+
+	}
 }
 
 static inline int normalization(luint m[4]){
@@ -148,7 +213,7 @@ static inline int normalization(luint m[4]){
 
 	//ohodnoti vrcholy
 	uint evaluation[N];
-	evaluation_2(adjacency_matrix,evaluation);
+	evaluation_3(adjacency_matrix,evaluation);
 
 /*	for (int i=0; i<N; i++)
 		printf("%d ",evaluation[i]);
@@ -338,7 +403,6 @@ int minimax_threat(int player, luint m[4], int depth, int hash, int u, int v){
 
 
 int minimax(int player, luint m[4], int depth, int hash){
-	//TODO resit vynucene tahy
 	//1 zeleny vyhraje 
 	//0 remiza obarvene bez k4 
 	//-1 cerveny vyhraje
