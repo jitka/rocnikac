c35d3542c04059327ba473144bae2f254a2276d4
Jitka Novotna <jitka@ucw.cz>
2012-03-29 10:57:50 +0200

    spojak netestovany

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "linkedlist.h"
+
+ll_t* llNew(){
+	return NULL;
+}
+
+void llAddNode(ll_t** where, node_t* node){
+	ll_t* item = malloc(sizeof(ll_t));
+	if (item == NULL)
+		perror("malloc item");
+	item->data = node;
+	item->next = *where;
+	*where = item;
+}
+
+node_t* llGetNode(ll_t** where){
+	if (*where == NULL)
+		return NULL;
+	ll_t* item = *where;
+	*where = item->next;
+	node_t* data = item->data;
+	free(item);
+	return data;
+}
--- src/linkedlist.h
+++ src/linkedlist.h
@@ -1,8 +1,17 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-//typedef struct node node_t;
-//struct node{}
+#include "node.h"
+
+typedef struct linkedListNode ll_t;
+struct linkedListNode{
+	node_t* data;
+	ll_t* next;
+};
+
+ll_t* llNew();
+void llAddNode(ll_t** where, node_t* node);
+node_t* llGetNode(ll_t** where); //kdyz neni vraci NULL
 
 #endif
 
